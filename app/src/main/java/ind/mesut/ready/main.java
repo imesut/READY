8db4e50e69f109944a1976586fe584e81d2ae25a
Mesut Yılmaz <mesut.yilmaz@yga.org.tr>
2018-02-11 21:04:37 +0300

    init

---
+++ app/src/main/java/ind/mesut/ready/main.java
@@ -0,0 +1,13 @@
+package ind.mesut.ready;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class main extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
