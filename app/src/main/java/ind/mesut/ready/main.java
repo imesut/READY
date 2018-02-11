3276f9b3ce1c66841e9d545cf44302241fa66df5
Mesut Yılmaz <mesut.yilmaz@yga.org.tr>
2018-02-11 22:21:21 +0300

    start-stop-speed function

--- app/src/main/java/ind/mesut/ready/main.java
+++ app/src/main/java/ind/mesut/ready/main.java
@@ -1,7 +1,13 @@
 package ind.mesut.ready;
 
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 public class main extends AppCompatActivity {
 
@@ -9,5 +15,45 @@ public class main extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Button startButton = (Button) findViewById(R.id.playButton);
+        final SeekBar speed = (SeekBar) findViewById(R.id.speed);
+        final MediaPlayer mP = MediaPlayer.create(this, R.raw.sample);
+        final TextView textV = (TextView) findViewById(R.id.textView);
+
+        speed.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                float speed = 0.75f+i*0.25f;
+                textV.setText(Float.toString(speed));
+                if(mP.isPlaying()){
+                    mP.setPlaybackParams(mP.getPlaybackParams().setSpeed(speed));
+                }
+            }
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {}
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {}
+        });
+
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d("position: ", Integer.toString(mP.getCurrentPosition()));
+                Log.d("position: ", Integer.toString(mP.getDuration()));
+
+                if (mP.isPlaying()){
+                    mP.stop();
+                    try{
+                        mP.prepare();
+                    } catch (Exception e){};
+
+                    startButton.setText("Start");
+                }else{
+                    mP.start();
+                    startButton.setText("Stop");
+                }
+            }
+        });
     }
 }
