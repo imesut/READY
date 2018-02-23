db851046c83ccf77a760bb7ad94faa742121e318
Mesut Yılmaz <mesut.yilmaz@yga.org.tr>
2018-02-23 22:05:25 +0300

    Single Option Trigger

--- app/src/main/java/ind/mesut/ready/main.java
+++ app/src/main/java/ind/mesut/ready/main.java
@@ -1,10 +1,12 @@
 package ind.mesut.ready;
 
 import android.app.VoiceInteractor;
+import android.content.Context;
 import android.content.Intent;
 import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Message;
+import android.os.StrictMode;
 import android.provider.AlarmClock;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -17,6 +19,9 @@ import android.widget.Toast;
 import android.app.VoiceInteractor;
 import android.app.VoiceInteractor.PickOptionRequest;
 import android.app.VoiceInteractor.PickOptionRequest.Option;
+
+import java.util.Map;
+import java.util.StringTokenizer;
 
 public class main extends AppCompatActivity {
 
@@ -56,7 +61,7 @@ public class main extends AppCompatActivity {
             public void onClick(View view) {
                 Log.d("position: ", Integer.toString(mP.getCurrentPosition()));
                 Log.d("position: ", Integer.toString(mP.getDuration()));
-
+                /*
                 if (mP.isPlaying()){
                     mP.stop();
                     try{
@@ -64,6 +69,15 @@ public class main extends AppCompatActivity {
                     } catch (Exception e){};
 
                     startButton.setText("Start");
+                }else{
+                    mP.start();
+                    startButton.setText("Stop");
+                }
+                */
+                if (mP.isPlaying()){
+                    mP.pause();
+                    startButton.setText("Start");
+
                 }else{
                     mP.start();
                     startButton.setText("Stop");
@@ -76,6 +90,38 @@ public class main extends AppCompatActivity {
     protected void onDestroy() {
         super.onDestroy();
         Log.d("mode:", "ondestroy");
+    }
+
+    public void doOp(Integer optionId){
+        Log.d("doOp", String.valueOf(optionId));
+        if (mP.isPlaying()){
+            mP.pause();
+
+        }else{
+            mP.start();
+        }
+    }
+
+    void Prompt(Option[] options, String promptText){
+        VoiceInteractor.Prompt prompt = new VoiceInteractor.Prompt(promptText);
+
+        this.getVoiceInteractor().submitRequest(
+                new PickOptionRequest(prompt, options, null) {
+                    @Override
+                    public void onPickOptionResult(boolean finished, Option[] selections, Bundle result) {
+                        Log.d("selections", selections.toString());
+                        if (finished && selections.length == 1) {
+                            Log.d("selections", selections.toString());
+                            Log.d("id", String.valueOf(selections[0].getIndex()));
+                            Log.d("text", String.valueOf(selections.toString()));
+                            doOp(selections[0].getIndex());
+                        }
+                    }
+                    @Override
+                    public void onCancel() {
+                        Log.d("voice", "cancel");
+                    }
+                }, "MainPrompt");
     }
 
     @Override
@@ -91,33 +137,52 @@ public class main extends AppCompatActivity {
 
         Log.d("state:", "onResume");
 
+        // String[][] MainOptions = new String[][] {};
+
+        /*
+        String[] mainOption1 =  {"1", "bana kitap oku", "read a book", "begin", "play", "pause"};
+        String[] mainOption2 =  {"2", "second", "end", "view"};
+        String[] exit =  {"exit", "stop"};
+        String[] option1Play = {"back", "before"};
+        String[] option2Play = {"forward"};
+        */
+
+        // MainOptions[0] = mainOption1;
+        // MainOptions[1] = mainOption2;
+
         if (isVoiceInteraction()) {
-            VoiceInteractor.PickOptionRequest.Option option1 = new VoiceInteractor.PickOptionRequest.Option("Light", 0);
-            option1.addSynonym("White");
-            option1.addSynonym("Jedi");
-            VoiceInteractor.PickOptionRequest.Option option2 = new VoiceInteractor.PickOptionRequest.Option("Dark", 1);
-            option2.addSynonym("Black");
-            option2.addSynonym("Sith");
 
-            Option[] options = new Option[]{option1, option2};
-            VoiceInteractor.Prompt prompt = new VoiceInteractor.Prompt("Which side are you on");
+            Message message = Message.obtain();
+            Log.d("a", String.valueOf(message.obj));
 
-            this.getVoiceInteractor().submitRequest(new PickOptionRequest(prompt, options, null)
-            {
-                @Override
-                public void onPickOptionResult(boolean finished, Option[] selections, Bundle result) {
-                    if (finished && selections.length == 1) {
-                        if (selections[0].getIndex() == 0) Log.d("optionselected", "light");
-                        else if (selections[0].getIndex() == 1) Log.d("optionselected", "dark");
-                    }
-                }
+            /*
+            VoiceInteractor.PickOptionRequest.Option mO1 = new VoiceInteractor.PickOptionRequest.Option("Play", 0);
+            VoiceInteractor.PickOptionRequest.Option mO2 = new VoiceInteractor.PickOptionRequest.Option("View", 1);
 
-                @Override
-                public void onCancel() {
-                    Log.d("voice", "cancel");
-                }
+
+            for (String option : mainOption1) {
+                mO1.addSynonym(option);
             }
-            , "Theme Selector");
+            for (String option : mainOption2) {
+                mO2.addSynonym(option);
+            }
+
+            Option[] options = new Option[]{mO1, mO2};
+            */
+
+            VoiceInteractor.PickOptionRequest.Option option = new VoiceInteractor.PickOptionRequest.Option("play", 0);
+            option.addSynonym("ready");
+            option.addSynonym("go");
+            option.addSynonym("take it");
+            option.addSynonym("ok");
+            option.addSynonym("pause");
+
+            Option[] optionL = new Option[]{option};
+
+            Prompt(optionL, "What do you want to do? Play or View?");
+
+            //TODO: Play / Pause
+            //TODO: Custom intent
 
         }
     }
